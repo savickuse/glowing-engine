8a8997155a5dab81e0f914c22fead1fff6445a48
savickuse <savickuse@users.noreply.github.com>
2025-11-17 23:04:55 +0200

    improve: add error handling

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 44
 module.exports = API;
+
+
+// API module for EngineCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 45
+module.exports = API;
