2bf1cadee321fb8999f94745070c529dc7fd80e3
savickuse <savickuse@users.noreply.github.com>
2025-12-18 14:40:25 +0200

    improve: fix async handling

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 40
 module.exports = API;
+
+
+// API module for EngineCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 41
+module.exports = API;
