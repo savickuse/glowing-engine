a15c2c944cac7250547456afa2b0839c2335b959
savickuse <savickuse@users.noreply.github.com>
2026-01-13 12:54:31 +0200

    improve: improve code readability

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 13
 module.exports = API;
+
+
+// API module for EngineCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 20
+module.exports = API;
