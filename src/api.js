86bfbf62770304c898e6074f0eac43bee85f94ef
savickuse <savickuse@users.noreply.github.com>
2026-01-24 15:01:40 +0200

    fix: update error messages

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 20
 module.exports = API;
+
+
+// API module for EngineCore
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 40
+module.exports = API;
